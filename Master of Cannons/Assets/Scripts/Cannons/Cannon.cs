eeb33a5cc91609c6692c71d66236002a50a5bb07
JuanD2011 <juan.atehortual@upb.edu.co>
2019-09-05 01:04:00 -0500

    [WIP] Cannons behaviour & 3rd party

---
+++ Master of Cannons/Assets/Scripts/Cannons/Cannon.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    [SerializeField]
+    protected float shootForce = 10f, wickLength = 5f;
+
+    protected bool burningWick = false;
+    protected float elapsedWickTime = 0f;
+
+    protected virtual void Update()
+    {
+        if (burningWick) elapsedWickTime += Time.deltaTime;
+
+        if (elapsedWickTime > wickLength) Shoot();
+    }
+
+    protected virtual void Shoot()
+    {
+        burningWick = false;
+    }
+
+    protected void CatchCharacter()
+    {
+        elapsedWickTime = 0f;
+        burningWick = true;
+    }
+
+    protected void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Character")) CatchCharacter();
+    }
+}
---
+++ Master of Cannons/Assets/Scripts/Cannons/MovingCannon.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class MovingCannon : Cannon
+{
+    [SerializeField]
+    private bool startMoving = false;
+
+    [SerializeField]
+    Transform target1 = null, target2 = null;
+
+    [SerializeField]
+    private float speed = 5f;
+
+    [SerializeField]
+    private iTween.EaseType easeType;
+
+    private bool verticalCannon = false;
+
+    private void Start()
+    {
+        //if (target1.position.y > transform.position.y || target2.position.y > transform.position.y || target1.position.y < transform.position.y || target2.position.y < transform.position.y) verticalCannon = true;
+        //else verticalCannon = false;
+
+        target1.SetParent(null);
+        target2.SetParent(null);
+
+        if (startMoving) Move();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    private void Move()
+    {
+        MoveToFirstTarget();
+    }
+
+    private void MoveToFirstTarget()
+    {
+        if (!verticalCannon)
+        {
+            iTween.MoveTo(gameObject, iTween.Hash("position", target1.position, "easeType", easeType, "speed", speed, "oncomplete", "MoveToSecondTarget"));
+        }
+        else
+        {
+            iTween.MoveTo(gameObject, iTween.Hash("position", target1.position, "easeType", easeType, "speed", speed, "oncomplete", "MoveToSecondTarget"));
+        }
+    }
+
+    private void MoveToSecondTarget()
+    {
+        if (!verticalCannon)
+        {
+            iTween.MoveTo(gameObject, iTween.Hash("position", target2.position, "easeType", easeType, "speed", speed, "oncomplete", "MoveToFirstTarget"));
+        }
+        else
+        {
+            iTween.MoveTo(gameObject, iTween.Hash("position", target2.position, "easeType", easeType, "speed", speed, "oncomplete", "MoveToFirstTarget"));
+        }
+    }
+}
