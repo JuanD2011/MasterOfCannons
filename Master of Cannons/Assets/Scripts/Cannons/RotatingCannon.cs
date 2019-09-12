feb0b42eb2ce78676b61d42939b6286182012d90
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-09-12 16:52:30 -0500

    Rotating Cannon

---
+++ Master of Cannons/Assets/Scripts/Cannons/RotatingCannon.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class RotatingCannon : MovingCannon
+{
+    [SerializeField] iTween.LoopType loopType = iTween.LoopType.none;
+
+    [Tooltip("Will find the shortest path to reach the angle")]
+    [SerializeField] Vector3[] angles = new Vector3[2];
+
+    int anglesCounter = 0;
+
+    void Start()
+    {
+        if (startMoving) Move();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+
+    protected override void Move()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        iTween.RotateTo(gameObject, iTween.Hash("rotation", angles[anglesCounter], "easeType", easeType, "speed", speed, "ignoretimescale", true, "looptype", loopType, "oncomplete", "Rotate"));
+        anglesCounter = (anglesCounter + 1) % angles.Length;
+    }
+}
