da3bd09b226f454f037e713932765ea62a325c33
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-09-13 20:06:40 -0500

    UI main panels animation

---
+++ Master of Cannons/Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] panels = new GameObject[0];
+
+    Animator[] panelAnimators = new Animator[0];
+
+    int currentPanelIndex = 0;
+
+    private readonly string panelFadeIn = "MP Fade-in";
+    private readonly string panelFadeOut = "MP Fade-out";
+    private readonly string panelFadeInStart = "MP Fade-in Start";
+
+    private void Awake()
+    {
+        panelAnimators = new Animator[panels.Length];
+
+        for (int i = 0; i < panelAnimators.Length; i++)
+        {
+            panelAnimators[i] = panels[i].GetComponent<Animator>();
+        }
+    }
+
+    void Start()
+    {
+        panelAnimators[currentPanelIndex].Play(panelFadeInStart);
+    }
+
+    /// <summary>
+    /// Fade out the current panel and fade in the panel provided by _newPanelIndex
+    /// </summary>
+    /// <param name="_newPanelIndex"></param>
+    public void PanelAnim(int _newPanelIndex)
+    {
+        if (_newPanelIndex != currentPanelIndex)
+        {
+            panelAnimators[currentPanelIndex].Play(panelFadeOut);
+            currentPanelIndex = _newPanelIndex;
+            panelAnimators[currentPanelIndex].Play(panelFadeIn);
+        }
+    }
+}
