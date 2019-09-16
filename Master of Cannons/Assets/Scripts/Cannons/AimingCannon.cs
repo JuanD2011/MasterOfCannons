379215b5e455f64bff8a10586088a48687a142db
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-09-15 20:30:07 -0500

    Aiming cannon

--- Master of Cannons/Assets/Scripts/Cannons/AimingCannon.cs
+++ Master of Cannons/Assets/Scripts/Cannons/AimingCannon.cs
@@ -5,6 +5,10 @@ public class AimingCannon : Cannon
     float targetRotation = 0f;
     float turnSmoothVel = 0f, turnSmooth = 0.15f;
 
+    Vector3 direction = Vector3.zero;
+
+    Camera m_Camera = null;
+
     protected override void Awake()
     {
         base.Awake();
@@ -15,14 +19,13 @@ public class AimingCannon : Cannon
     {
         base.Start();
         PlayerInputHandler.OnAimAction += AimCannon;
+        m_Camera = Camera.main;
     }
 
     private void AimCannon(Vector2 _AimVector)
     {
-        if (_AimVector != Vector2.zero)
-        {
-            targetRotation = Mathf.Atan2(-_AimVector.x, _AimVector.y) * Mathf.Rad2Deg;
-            transform.eulerAngles = Vector3.forward * Mathf.SmoothDampAngle(transform.eulerAngles.z, targetRotation, ref turnSmoothVel, turnSmooth); 
-        }
+        direction = new Vector3(_AimVector.x, _AimVector.y, 0) - m_Camera.WorldToScreenPoint(transform.position);
+        targetRotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(targetRotation - 90, Vector3.forward);
     }
 }
