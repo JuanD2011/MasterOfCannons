7b10d82d353e564c3380a6069806f7134d8a3060
JuanD2011 <juan.atehortual@upb.edu.co>
2019-10-23 22:57:42 -0500

    [WIP] Rotating shield boss

--- Master of Cannons/Assets/Scripts/Bosses/Boss.cs
+++ Master of Cannons/Assets/Scripts/Bosses/Boss.cs
@@ -5,6 +5,16 @@ public abstract class Boss : MonoBehaviour
     [SerializeField]
     protected float life = 0f;
 
+    [SerializeField, Range(1, 10)]
+    protected byte difficulty = 1;
+
     [SerializeField]
     protected Character character = null;
+
+    protected virtual void Awake()
+    {
+        SetDifficulty();
+    }
+
+    protected abstract void SetDifficulty();
 }
