8b0b35a6e3f9fcee306943f0fa3bdaa0f7d69946
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-12-08 15:15:12 -0500

    [WIP] Mockup UI

---
+++ Master of Cannons/Assets/Scripts/UI/FakeModelRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FakeModelRotation : MonoBehaviour, IDragHandler
+{
+    public static event Delegates.Action<float> OnRotate = null;
+
+    public void OnDrag(PointerEventData _EventData)
+    {
+        OnRotate( _EventData.delta.x);
+    }
+}
