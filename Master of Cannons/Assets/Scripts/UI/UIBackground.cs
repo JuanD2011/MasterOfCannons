b51d96948c2c18a48f74d2ba65256d2b92cd0b11
Daniel <daniel.alarcon@timba.co>
2019-12-14 18:39:56 -0500

    Background, Loading screen

---
+++ Master of Cannons/Assets/Scripts/UI/UIBackground.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class UIBackground : MonoBehaviour
+{
+    /// <summary>
+    /// Intercalate between active and inactive
+    /// </summary>
+    public void IntercalateActiveGameObject()
+    {
+        if (gameObject.activeInHierarchy)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
