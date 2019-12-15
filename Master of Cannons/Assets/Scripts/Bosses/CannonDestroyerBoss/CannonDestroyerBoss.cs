aa8ced63aa08691d2a95f383962109bf4ce63b39
JuanDAte <juan.atehortua@timba.co>
2019-12-14 19:49:39 -0500

    Initial commit

---
+++ Master of Cannons/Assets/Scripts/Bosses/CannonDestroyerBoss/CannonDestroyerBoss.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CannonDestroyerBoss : Boss
+{
+    [SerializeField]
+    private GameObject cannonsParent = null;
+
+    [SerializeField]
+    private float bulletLifeTime = 0f;
+
+    private Cannon[] cannons;
+
+    private void Start()
+    {
+        cannons = cannonsParent.GetComponentsInChildren<Cannon>();
+    }
+
+    protected override void SetDifficulty()
+    {
+
+    }
+}
---
+++ Master of Cannons/Assets/Scripts/Bosses/CannonDestroyerBoss/CannonDestroyerBullet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CannonDestroyerBullet : MonoBehaviour
+{
+    private static float lifeTime = 0f;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void Deactivate()
+    {
+
+    }
+}
