ac0542aa7128e4c04721b5736d57fb9260ecd127
JuanDAte <juan.atehortua@timba.co>
2019-12-28 19:58:29 -0500

    Airstream switcher

--- Master of Cannons/Assets/Scripts/Cannons/AirStreamCannon/AirStreamSwitchBehaviour.cs
+++ Master of Cannons/Assets/Scripts/Cannons/AirStreamCannon/AirStreamSwitchBehaviour.cs
@@ -6,4 +6,16 @@ public class AirStreamSwitchBehaviour : MonoBehaviour
     [SerializeField] private float timeToChange = 0f;
 
     public event Delegates.Action OnSwich = null;
+
+    private float time = 0f;
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        if (time > timeToChange)
+        {
+            time = 0f;
+            OnSwich?.Invoke();
+        }
+    }
 }
