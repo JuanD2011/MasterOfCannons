fc6ef24cad27aab82eefeb5cdfc06d039ecc5def
JuanDAte <juan.atehortua@timba.co>
2019-12-28 19:49:30 -0500

    AirStream cannon w/ switch behaviour & particles

---
+++ Master of Cannons/Assets/Scripts/Cannons/AirStreamCannon/AirStreamSwitchBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AirStreamBehaviour))]
+public class AirStreamSwitchBehaviour : MonoBehaviour
+{
+    [SerializeField] private float timeToChange = 0f;
+
+    public event Delegates.Action OnSwich = null;
+}
